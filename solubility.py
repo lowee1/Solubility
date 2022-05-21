8b6781b1949703a59dec6b471f767569127530c2
lowee1 <63391242+lowee1@users.noreply.github.com>
2022-05-21 18:07:37 +0800

    printing format changes

--- solubility.py
+++ solubility.py
@@ -1,5 +1,13 @@
 import random
 import click
+import shutil
+
+columns = shutil.get_terminal_size().columns
+
+
+def center(text: str):
+    """shifts the text towards the center in the terminal"""
+    return " " * (columns // 3) + text
 
 
 class Ion:
@@ -80,29 +88,31 @@ def check_solubility(cation: Ion, anion: Ion) -> bool:
 
 
 def solubility_enquiry():
-    """asks the user for a cation and anion and click.echos whether the compound is soluble or not"""
+    """asks the user for a cation and anion and center_echos whether the compound is soluble or not"""
     cation_names = [c.name for c in cations]
     anion_names = [a.name for a in anions]
 
+    click.echo()  # newline
+
     cation = ""
     while cation not in cation_names:
-        cation = input("Enter cation: ")
+        cation = input(center("Enter cation: "))
         if cation not in cation_names:
-            click.echo(click.style("Invalid cation", fg="orange"))
+            click.echo(center(click.style("Invalid cation", fg="orange")))
 
     anion = ""
     while anion not in anion_names:
-        anion = input("Enter anion: ")
+        anion = input(center("Enter anion: "))
         if anion not in anion_names:
-            click.echo(click.style("Invalid anion", fg="orange"))
+            click.echo(center(click.style("Invalid anion", fg="orange")))
 
     cation: Ion = [c for c in cations if c.name == cation][0]
     anion: Ion = [a for a in anions if a.name == anion][0]
 
     if check_solubility(cation, anion):
-        click.echo("The compound is soluble in water.")
+        click.echo(center("The compound is soluble in water."))
     else:
-        click.echo("The compound is not soluble in water.")
+        click.echo(center("The compound is not soluble in water."))
 
 
 def solubility_quiz():
@@ -111,45 +121,50 @@ def solubility_quiz():
     score = 0
     question_count = 0
     while True:
+        click.echo()  # newline
+        
         # generate a random anion and cation
         anion = random.choice(anions)
         cation = random.choice(cations)
 
-        # click.echo the question
-        click.echo("Is " + str(cation) + " " + str(anion) + " soluble in water?")
+        # center_echo the question
+        click.echo(
+            center("Is " + str(cation) + " " + str(anion) + " soluble in water?")
+        )
 
         # get the user's answer
-        answer = input("Enter yes or no: ")
+        answer = ""
+        while answer not in ["y", "n"]:
+            answer = input(center("Enter y or n: "))
+            if answer not in ["y", "n"]:
+                click.echo(center(click.style("Invalid answer", fg="yellow")))
 
         # check if the answer is correct
-        if (check_solubility(cation, anion) and answer == "yes") or (
-            not check_solubility(cation, anion) and answer == "no"
+        if (check_solubility(cation, anion) and answer == "y") or (
+            not check_solubility(cation, anion) and answer == "n"
         ):
-            click.echo(click.style("Correct!", fg="green"))
+            click.echo(center(click.style("Correct!", fg="green")))
             score += 1
         else:
-            click.echo(click.style("Incorrect!", fg="red"))
+            click.echo(center(click.style("Incorrect!", fg="red")))
 
         question_count += 1
         if question_count == 10:
             # ask if the user wants to continue
-            answer = input("Continue? (y/n) ")
+            answer = input(center("Continue? (y/n) "))
             if answer == "n":
-                click.echo("You scored " + str(score))
+                click.echo(center("You scored " + str(score)))
                 break
         question_count %= 10
 
 
 while True:
     # ask if the user wants to check individual compounds or do a quiz
-    click.echo(
-        """
-    1. Check individual compounds
-    2. Do a quiz
-    3. Exit
-    """
-    )
-    choice = input("Enter your choice: ")
+    click.echo(center("1. Check individual compounds"))
+    click.echo(center("2. Solubility quiz"))
+    click.echo(center("3. Exit\n"))
+
+    choice = input(center("Enter your choice: "))
     if choice == "1":
         solubility_enquiry()
     elif choice == "2":
@@ -157,4 +172,4 @@ while True:
     elif choice == "3":
         break
     else:
-        click.echo("Invalid choice.")
+        click.echo(center("Invalid choice."))
